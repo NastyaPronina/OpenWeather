08e6c36c1d9f2c43e51a15dbe7632e4113b341d6
Anastasiya Pronina <apronina@piesoft.us>
2023-04-19 20:09:18 +0300

    Added first tests

---
+++ conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+@pytest.fixture(scope="session")
+def driver():
+    print("start browser")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.maximize_window()
+    yield driver
+    driver.quit()
